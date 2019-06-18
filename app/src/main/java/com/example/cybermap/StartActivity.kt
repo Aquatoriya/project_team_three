343013822b14f2e2fbfd32d5da05b0570a1b6dc8
Aquatoriya <45881490+Aquatoriya@users.noreply.github.com>
2019-06-18 14:27:11 +0300

    Create StartActivity.kt

---
+++ app/src/main/java/com/example/cybermap/StartActivity.kt
@@ -0,0 +1,19 @@
+package com.example.cybermap
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start.*
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        button_start.setOnClickListener() {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
